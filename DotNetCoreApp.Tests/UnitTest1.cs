2a96c954b18cb99758197d8059232b3b403a10d7
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-09 18:54:11 +0530

    new ci cd pipeline

---
+++ DotNetCoreApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+public class HelloControllerTests
+{
+    [Fact]
+    public void Get_ReturnsExpectedMessage()
+    {
+        var controller = new HelloController();
+        var result = controller.Get();
+        Assert.Equal("Hello, DevOps World!", result);
+    }
+}
