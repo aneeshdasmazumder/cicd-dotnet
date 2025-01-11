cbae65966517a74d089ebeddda15dfd4dfdec637
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-12 02:57:20 +0530

    cicd-pipeline terraform details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,9 @@
 provider "aws" {
-  region  = "ap-south-1"
-  version = "~> 3.0"
+  region = "ap-south-1"
 }
 
-
-# Create an S3 bucket
 resource "aws_s3_bucket" "example" {
   bucket = "my-cicdpipeline-bucket-unique-12345"  # Replace with a unique bucket name
 
-  # Block public access settings for the bucket
-  block_public_access {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = false
-    restrict_public_buckets = true
-  }
+  acl    = "private"  # Set ACL directly here
 }
