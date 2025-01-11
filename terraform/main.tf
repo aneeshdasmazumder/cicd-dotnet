37b90932580b521ed2043a084c52a30c57693d9b
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-12 03:01:53 +0530

    cicd-pipeline terraform details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,12 @@
-provider "aws" {
-  region = "ap-south-1"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-cicdpipeline-bucket-unique-${random_id.bucket_suffix.hex}"  # Ensure unique name
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-cicdpipeline-bucket-unique-12345"  # Replace with a unique bucket name
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
 
-  acl    = "private"  # Set ACL directly here
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.bucket
+  acl    = "private"
 }
