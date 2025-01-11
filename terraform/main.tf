21190aa9ed044208c896633831b422c8b5d0f9c9
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-12 02:38:43 +0530

    cicd-pipeline terraform details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "ap-south-1"  # Asia Pacific (Mumbai)
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "my-cicdpipeline-bucket-unique-12345"  # Ensure this is globally unique
 }
