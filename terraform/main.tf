7538b7582d6322e950010f1dfa0d94e6d65e1bea
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-13 01:21:20 +0530

    cicd-pipeline-terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,3 @@ resource "aws_s3_bucket_policy" "example_bucket_policy" {
     ]
   })
 }
-
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.bucket
-  acl    = "private"
-}
