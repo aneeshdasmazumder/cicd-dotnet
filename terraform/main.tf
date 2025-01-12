c5dc7dcaca6ad296cd892bf27d9009d99b044b0b
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-13 01:28:42 +0530

    cicd-pipeline-terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-cicdpipeline-bucket-unique-${random_id.bucket_suffix.hex}"  # Ensure unique name
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls   = false
+  ignore_public_acls  = false
+  block_public_policy = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "example_bucket_policy" {
   bucket = aws_s3_bucket.example.id
 
