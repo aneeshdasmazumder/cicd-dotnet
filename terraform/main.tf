89f6b968b3695470cdf1724f4571231f51cebf78
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-13 22:52:35 +0530

    cicd-pipeline-build

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "db" {
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   engine               = "mysql"
-  engine_version       = "8.0.26"  # Supported engine version
+  engine_version       = "8.0.34"  # Supported engine version
   instance_class       = "db.t3.micro"  # Supported instance class
   db_name              = "mydatabase"  # Corrected argument name
   username             = "admin"
