4a7922dad1fd273a634c8848be4babb5bc1d527a
Aneesh Das Mazumder <aneesh.mazumder@hyland.com>
2025-01-17 18:07:16 +0530

    cicd-pipeline-for-deployment

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,1 @@
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
-
-output "app_public_ip" {
-  value = aws_instance.app.public_ip
-}
-
-output "db_endpoint" {
-  value = aws_db_instance.default.endpoint
-}
+# This file is no longer needed as the outputs are already defined in main.tf.
